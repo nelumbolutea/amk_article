67eed674cd2d05b7c1a77749e7cff2009a005bb2
nelumbolutea <93508648+nelumbolutea@users.noreply.github.com>
2022-04-07 14:12:00 +0800

    Add files via upload

---
+++ 6.CIP_CALP.pl
@@ -0,0 +1,32 @@
+use warnings;
+use strict;
+use Bio::SearchIO;
+
+my $infile  = shift;
+
+my $parser = Bio::SearchIO->new(-file   => $infile,
+                                -format => 'blast');
+while( my $result = $parser->next_result ) {
+    print "Query: ",$result->query_name,"\tlength: ",$result->query_length,
+          "\tnumber of hits: ",$result->num_hits,"\n";
+    while(my $hit=$result->next_hit){
+        print "\tHit: ",$hit->name,"\tlength: ",$hit->hit_length,
+                  "\te-value: ",$hit->significance,"\n";
+        my $al=0;
+        my @id=();
+        while(my $hsp=$hit->next_hsp){
+            print "\t\tHsp length: ",$hsp->hsp_length,"\tID length: ",
+                          $hsp->num_identical,"\t idenity: ",
+                          $hsp->percent_identity,"\n";
+            $al=$al+($hsp->hsp_length);
+            my $ai=($hsp->num_identical)/($hsp->hsp_length);
+            push @id,$ai;
+        }
+        my $cip=0;
+        foreach (@id){
+            $cip=$cip+($_/$al*100);
+            }
+        my $calp=$al/($result->query_length);
+        print "\tcip: ",$cip,"\tcalp: ",$calp,"\n";
+    }
+}
